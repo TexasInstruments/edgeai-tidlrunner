f021d5aafc461bbea38b0dfec9177820023ffd92
Manu <a0393608@udtensorlab9>
2025-08-20 16:59:11 +0530

    pipeline_type added in extract

--- edgeai_tidlrunner/runner/modules/vision/settings/settings_default.py
+++ edgeai_tidlrunner/runner/modules/vision/settings/settings_default.py
@@ -176,6 +176,7 @@ COPY_SETTINGS_DEFAULT['optimize'] = COPY_SETTINGS_DEFAULT['basic'] | {
 
 ##########################################################################
 SETTINGS_DEFAULT['extract'] = SETTINGS_DEFAULT['basic'] | {
+    'pipeline_type':          {'dest': 'common.pipeline_type', 'default': 'extract', 'type': str, 'metavar': 'value', 'help': 'type of pipeline to run'}, 
     'mode':                   {'dest': 'common.extract.mode', 'default': 'submodules', 'type': str, 'metavar': 'value', 'choices': ['submodules', 'submodule', 'start2end', 'operators']},
     'submodule_name':         {'dest': 'common.extract.submodule_name', 'default': None, 'type': str, 'metavar': 'value'},
     'max_depth':              {'dest': 'common.extract.max_depth', 'default': 3, 'type': int, 'metavar': 'value'},
