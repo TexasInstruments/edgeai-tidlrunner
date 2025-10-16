9adea6d1ce2a405be099926c06e061f3586d42f3
Manu <a0393608@udtensorlab9>
2025-10-16 12:19:25 +0530

    cmd args update

--- edgeai_tidlrunner/runner/common/settings/settings_default.py
+++ edgeai_tidlrunner/runner/common/settings/settings_default.py
@@ -253,7 +253,7 @@ SETTINGS_DEFAULT['convert'] = SETTINGS_DEFAULT['basic'] | {
     'work_path':              {'dest': 'common.work_path', 'default':'./work_dirs/{pipeline_type}/{target_device}/{tensor_bits}bits', 'type':str, 'metavar':'value', 'help':'work path'},   
     'run_dir':            {'dest': 'session.run_dir', 'default':'{work_path}/{model_id}_{runtime_name}_{model_path}_{model_ext}', 'type':str, 'metavar':'value', 'help':'run_dir'},
     'pipeline_type':          {'dest': 'common.pipeline_type', 'default': 'convert', 'type': str, 'metavar': 'value', 'help': 'type of pipeline to run'},    
-    'output_model_path':      {'dest': 'common.output_model_path', 'default': None, 'type': str, 'group':'model', 'metavar': 'value', 'help': 'output model'},
+    'output_model_path':      {'dest': 'common.output_model_path', 'default': None, 'type': str, 'metavar': 'value', 'help': 'output model'},
     'model_selection':          {'dest': 'common.model_selection', 'default': None, 'type': str, 'metavar': 'value', 'help': 'select a subset of models to run - path of the model is compared using this model_selection regex to select a particular model or not'},
     'model_shortlist':          {'dest': 'common.model_shortlist', 'default': None, 'type': str, 'metavar': 'value', 'help': 'select a subset of models to run - models configs with model_shortlist value <= this specified value will be used'},
 }
@@ -265,8 +265,8 @@ COPY_SETTINGS_DEFAULT['convert'] = COPY_SETTINGS_DEFAULT['basic'] | {
 ##########################################################################
 SETTINGS_DEFAULT['distill'] = SETTINGS_DEFAULT['compile'] | {
     'pipeline_type':            {'dest': 'common.pipeline_type', 'default': 'distill', 'type': str, 'metavar': 'value', 'help': 'type of pipeline to run'},    
-    'teacher_model_path':       {'dest': 'common.teacher_model_path', 'default': None, 'type': str, 'group':'model', 'metavar': 'value', 'help': 'teacher model'},
-    'output_model_path':        {'dest': 'common.output_model_path', 'default': None, 'type': str, 'group':'model', 'metavar': 'value', 'help': 'output model'},
+    'teacher_model_path':       {'dest': 'common.teacher_model_path', 'default': None, 'type': str, 'metavar': 'value', 'help': 'teacher model'},
+    'output_model_path':        {'dest': 'common.output_model_path', 'default': None, 'type': str, 'metavar': 'value', 'help': 'output model'},
     'calibration_frames':       {'dest': 'session.runtime_options.advanced_options:calibration_frames', 'default': 100, 'type': int, 'metavar': 'value', 'help': 'number of frames for quantization calibration'},
     'calibration_iterations':   {'dest': 'session.runtime_options.advanced_options:calibration_iterations', 'default': 5, 'type': int, 'metavar': 'value', 'help': 'number of calibration iterations'},
     'calibration_batch_size':   {'dest': 'session.runtime_options.advanced_options:calibration_batch_size', 'default': 16, 'type': int, 'metavar': 'value', 'help': 'number of calibration batch size'},
